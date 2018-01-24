613615f9c77e8d822d41c17b20a79cbcbee9cb05
MISRA Souvik <SMA10@dsone.3ds.com>
2018-01-24 16:11:42 +0530

    svkmsr6: Stting up Bootstrap Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AD Request Visualization App';
+  title = 'Adv Request Visualization App';
 }
