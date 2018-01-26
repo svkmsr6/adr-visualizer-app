74549516cef6920e5550962311f2a36eff825d25
Souvik Misra <svkmsr6@gmail.com>
2018-01-27 02:02:48 +0530

    @svkmsr6: Spec changes for AppModule

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,44 @@
 import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { FetchAdrService } from '../app/services/fetch-adr.service';
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+import * as more from 'highcharts/highcharts-more.src';
 import { AppComponent } from './app.component';
+import { AdrDashboardComponent } from './components/adr-dashboard/adr-dashboard.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        AdrDashboardComponent
       ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        ChartModule
+      ],
+      providers: [
+        FetchAdrService,
+        { provide: HIGHCHARTS_MODULES, useFactory: function () {
+          return [more];
+        } }
+      ]
     }).compileComponents();
   }));
+  
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'ADR REQUEST DASHBOARD'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+    expect(app.title).toEqual('ADR REQUEST DASHBOARD');
+  })); 
 });
