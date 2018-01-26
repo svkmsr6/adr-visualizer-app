161805f81f83932a91b09db1685d7fd298d55f81
Souvik Misra <svkmsr6@gmail.com>
2018-01-27 01:33:13 +0530

    Refactoring part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FetchAdrService } from '../app/services/fetch-adr.service';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
